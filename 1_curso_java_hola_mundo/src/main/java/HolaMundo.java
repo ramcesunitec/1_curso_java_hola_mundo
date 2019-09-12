15a5eaf70f1e2296889172543b7d345f7a041566
ramce <ramce@RAMCES-PC972212>
2019-09-12 15:43:23 -0500

    Proyectos 1 java

--- 1_curso_java_hola_mundo/src/main/java/HolaMundo.java
+++ 1_curso_java_hola_mundo/src/main/java/HolaMundo.java
@@ -1,8 +1,9 @@
 
 public class HolaMundo {
-    public static void main(String arg[]){
-        System.out.println("Hola mundo");
+
+    public static void main(String[] args) {
         
+        System.out.println("Hola mundo desde Java");
+       
     }
-   
 }
